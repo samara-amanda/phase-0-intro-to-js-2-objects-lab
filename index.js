5b56c152b414bc9c6b0933569821319551cfd721
Sam <samara.amanda01@gmail.com>
2021-03-12 15:43:04 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+const employee = {
+    name: "Samara", 
+    streetAddress: "505 Rutland"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee={...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+const newEmployee = updateEmployeeWithKeyAndValue(employee,"name", "Sam");
+
+newEmployee.name;
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,streetAddress,value){
+    employee[streetAddress]= "12 Broadway"
+    return employee;
+}
+function deleteFromEmployeeByKey(employee,name, value){
+    const newEmployee2={...employee};
+    newEmployee2[name] = value;
+    return newEmployee2;
+}
+const newEmployee2 =deleteFromEmployeeByKey("name", "Sam");
+
+newEmployee2.name;
+
+function destructivelyDeleteFromEmployeeByKey(employee,name,value){
+    employee[name] =value;
+    return employee;
+}
